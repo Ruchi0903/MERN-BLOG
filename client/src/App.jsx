f3b97c620663a9db4a9d9686163d1fcb6ef11e79
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2024-06-22 00:21:28 +0530

    create sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignUp />} />
-        <Route path="/sign-up" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
