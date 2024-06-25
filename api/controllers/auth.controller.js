fe6df1acf97f91fe7fd6d5b3b9a58bfbd437fd9a
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2024-06-26 00:20:48 +0530

    add admin fucntionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,26 +1,37 @@
-import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
-import { errorHandler } from "../utils/error.js";
-import jwt from "jsonwebtoken"
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
-    if (!username || !email || !password || !username === "" || email === "" || password === "") {
-        next(errorHandler(400, 'All fields are required'))
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username === '' ||
+        email === '' ||
+        password === ''
+    ) {
+        next(errorHandler(400, 'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({ username, email, password: hashedPassword })
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
 
     try {
         await newUser.save();
-        res.json("Signup successful")
+        res.json('Signup successful');
     } catch (error) {
         next(error);
     }
-}
+};
 
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
@@ -28,35 +39,43 @@ export const signin = async (req, res, next) => {
     if (!email || !password || email === '' || password === '') {
         next(errorHandler(400, 'All fields are required'));
     }
+
     try {
         const validUser = await User.findOne({ email });
         if (!validUser) {
-            return next(errorHandler(404, 'User not found'))
+            return next(errorHandler(404, 'User not found'));
         }
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) {
-            return next(errorHandler(400, 'Invalid password'))
+            return next(errorHandler(400, 'Invalid password'));
         }
+        const token = jwt.sign(
+            { id: validUser._id, isAdmin: validUser.isAdmin },
+            process.env.JWT_SECRET
+        );
 
-        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-
-        // separates the password from the rest.
         const { password: pass, ...rest } = validUser._doc;
 
-        res.status(200).cookie('access_token', token, { httpOnly: true }).json(rest)
-
+        res
+            .status(200)
+            .cookie('access_token', token, {
+                httpOnly: true,
+            })
+            .json(rest);
     } catch (error) {
         next(error);
     }
-}
-
+};
 
 export const google = async (req, res, next) => {
     const { email, name, googlePhotoUrl } = req.body;
     try {
         const user = await User.findOne({ email });
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign(
+                { id: user._id, isAdmin: user.isAdmin },
+                process.env.JWT_SECRET
+            );
             const { password, ...rest } = user._doc;
             res
                 .status(200)
@@ -78,7 +97,10 @@ export const google = async (req, res, next) => {
                 profilePicture: googlePhotoUrl,
             });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const token = jwt.sign(
+                { id: newUser._id, isAdmin: newUser.isAdmin },
+                process.env.JWT_SECRET
+            );
             const { password, ...rest } = newUser._doc;
             res
                 .status(200)
