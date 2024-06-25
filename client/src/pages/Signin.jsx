461f9d9a750eaabe1f06c63d977a72debc0ee8e3
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2024-06-26 01:34:29 +0530

    show user posts inside dashboard

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -114,4 +114,4 @@ const SignIn = () => {
   );
 }
 
-export default SignIn
+export default SignIn;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,12 @@
-import flowbite from "flowbite-react/tailwind"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
-}
+  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
+};
