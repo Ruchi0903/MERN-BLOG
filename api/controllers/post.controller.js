032e5b2b3658af211a0064a901cf0ccd26ae2ebd
Ruchi Yadav <121485567+Ruchi0903@users.noreply.github.com>
2024-06-26 01:43:27 +0530

    add delete post functionality to the dashboard

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -60,12 +60,24 @@ export const getposts = async (req, res, next) => {
     const lastMonthPosts = await Post.countDocuments({
       createdAt: { $gte: oneMonthAgo },
     });
-
+    
     res.status(200).json({
       posts,
       totalPosts,
       lastMonthPosts,
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deletepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to delete this post'));
+  }
+  try {
+    await Post.findByIdAndDelete(req.params.postId);
+    res.status(200).json('The post has been deleted');
   } catch (error) {
     next(error);
   }
